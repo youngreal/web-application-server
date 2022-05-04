1d214451c7e8ca104d869108e3db3e5b8a7c69da
youngreal <dudwls0505@naver.com>
2022-05-04 23:58:38 +0900

    feat: 요구사항3 POST방식으로 회원가입 해결

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,11 +6,11 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Map;
 
-import com.google.common.base.Utf8;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 import static util.IndexToken.requestSplit;
 
@@ -18,6 +18,7 @@ public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -40,45 +41,86 @@ public class RequestHandler extends Thread {
             String url = requestSplit(line);
             System.out.println("url =" + url);
 
+            byte[] body = "Hello World".getBytes();
 
-                byte[] body = "Hello World".getBytes();
+
          if(url.equals("/index.html")) {
              body = Files.readAllBytes(new File("./webapp" + url).toPath());
+             printAllLine(line,br);
+             ResponseBodyMethod( out, body);
+             return;
          }
+
+
          if(url.equals("/user/form.html")){
              body = Files.readAllBytes(new File("./webapp" + url).toPath());
+             printAllLine(line, br);
+             ResponseBodyMethod(out, body);
+             return;
          }
 
+            /**
+             * get 방식 회원가입할때
+             */
             if (url.contains("?")) {
                 String subUrl = url.substring(13);
-                System.out.println("subUrl =" + subUrl);
                 Map<String, String> parsedUrl = HttpRequestUtils.parseQueryString(subUrl);
-                User user = new User(parsedUrl.get("userId"),parsedUrl.get("password"),parsedUrl.get("name")
-                        ,parsedUrl.get("email"));
-
-                System.out.println("userId=" +user.getUserId());
-                System.out.println("userName= " +user.getName());
-                System.out.println("userEmail=" + user.getEmail());
-                System.out.println("userPassword=" + user.getPassword());
+                User user = new User(parsedUrl.get("userId"),parsedUrl.get("password"),parsedUrl.get("name"),parsedUrl.get("email"));
+                printAllLine(line,br);
+                ResponseBodyMethod(out, body);
+                return;
             }
 
+            /**
+             * post 방식 회원가입할때
+             */
+            if(url.equals("/user/create")){
 
-            while (!"".equals(line)) {
-                if (line == null) {
-                    return;
-                }
-                System.out.println("line=" + line);
-                line = br.readLine();
+//                String subUrl = url.substring(13);
+//                Map<String, String> parsedUrl = HttpRequestUtils.parseQueryString(subUrl);
+//                User user = new User(parsedUrl.get("userId"),parsedUrl.get("password"),parsedUrl.get("name"),parsedUrl.get("email"));
+//                System.out.println("parsedUrl =" + parsedUrl);
+
+                printAllLine(line, br);
+                String brBody = IOUtils.readData(br, 87);
+                System.out.println("brBody= " + brBody);
+                // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+                DataOutputStream dos = new DataOutputStream(out);
+                response200HeaderBody(dos, body.length, brBody);
+                Map<String, String> parsedUrl = HttpRequestUtils.parseQueryString(brBody);
+                User user = new User(parsedUrl.get("userId"),parsedUrl.get("password"),parsedUrl.get("name"),parsedUrl.get("email"));
+                responseBody(dos, body);
+
             }
 
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            if (printAllLine(line, br)) return;
+            ResponseBodyMethod(out, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
+
+
+
+    private boolean printAllLine(String line, BufferedReader br) throws IOException {
+        while (!"".equals(line)) {
+            if (line == null) {
+                return true;
+            }
+            System.out.println("line=" + line);
+            line = br.readLine();
+        }
+        return false;
+    }
+
+    private void ResponseBodyMethod(OutputStream out, byte[] body) throws IOException {
+        // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        DataOutputStream dos = new DataOutputStream(out);
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
@@ -86,6 +128,19 @@ public class RequestHandler extends Thread {
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200HeaderBody(DataOutputStream dos, int length, String body) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + length + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.writeBytes(body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
